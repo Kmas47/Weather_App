f93920655c215772c59fcf66477433e6f4647751
Kmas47 <56531225+Kmas47@users.noreply.github.com>
2020-11-03 12:26:16 -0500

    redux

--- my-app/src/location.js
+++ my-app/src/location.js
@@ -1,38 +1,27 @@
+import { useState } from 'react';
 import { connect } from 'react-redux';
-import store from './store/store';
-const { Component } = require("react"); 
-
-class UserLocation extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            city: ""
-        }
-    }
-
-componentDidMount() {
+import store from './store/store'; 
+  
+function UserLocation() {
+   const [state, setState] = useState({ city: "" });
    const url = `http://api.ipstack.com/check?access_key=${process.env.REACT_APP_KEY}&fields=city`;
-   const city = fetch(url).then(Response => Response.json());
-   city.then(prop => {
-       this.setState({ city: prop.city })  
-       const a = store.dispatch({type: 'INITIAL', payload: this.state.city}) 
-       console.log(a)
-       
-
+   const location = fetch(url).then(Response => Response.json());
+   location.then(prop => {
+       console.log(prop)
+       if (state.city !== "") {
+        setState({ city: prop.city })
+       } 
+        if(state.city !== "") {
+        store.dispatch({ type: 'INITIAL', payload: state.city }) 
+       }  
    }) 
-}
-
-render() {
-    
-    return(
+   return(
         
-        <div>
-            <h1>Location:{this.state.city}</h1>
-        </div>
-    );
+    <div>
+        <h1>Location:{state.city}</h1>
+    </div>
+);
 }
 
-}
-
-export default  connect (state => state)(UserLocation);
+export default connect (state => state)(UserLocation);
 
