725969be4846248b9cd9eb0a1d42a11c2817c84f
Kmas47 <56531225+Kmas47@users.noreply.github.com>
2020-11-03 22:19:37 -0500

    Final Changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,11 +9,11 @@ import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <ApolloProvider client={client}>
+      <Provider store={store}>
           <App />
+          </Provider>
       </ApolloProvider>
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
